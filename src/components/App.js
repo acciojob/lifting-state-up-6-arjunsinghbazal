aaeffcf4aef77024246bf7a5bc91fc74740ef542
Arjun Singh <55157623+arjunsinghbazal@users.noreply.github.com>
2023-08-30 12:17:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,51 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+const Child = (todo) => {
 
+    return (
+        <div>
+            <span>{todo.text}</span>
+        </div>
+    )
+}
+const TodoList = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Learn React", completed: false },
+    { id: 2, text: "Build a todo app", completed: false },
+    { id: 3, text: "Deploy to production", completed: false },
+  ]);
+
+  // Function to mark a todo item as completed
+  const completeTodo = (id) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, completed: true };
+      }
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          <Child todo={todo} />
+          {!todo.completed && (
+            <button onClick={() => completeTodo(todo.id)}>Complete</button>
+          )}
+        </li>
+      ))}
+    </div>
+  );
+};
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <TodoList/>
     </div>
   )
 }
